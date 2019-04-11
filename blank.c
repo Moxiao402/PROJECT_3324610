2dc06a76bfb4a5a794a4ecdf0f8522ea7c4a82d8
Mingyi Shao <c3324610@uon.edu.au>
2019-04-11 07:03:29 +0000

    Wrote codes for rotational cipher

--- blank.c
+++ blank.c
@@ -1,11 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
+    char encrypmessage[100], decrypmessage[100];
+    int i,k,shift,l;
+    int choice1,choice2;
+     
 int main(){
+
+
+     printf("Please choose which from which task you would like me to conduct, and input its number:\n");
+     printf("1. Encryption\n");
+     printf("2. Decryption\n");
     
-    printf("hello world");
+     scanf("%d", &choice1);
+     
+     if(choice1=1){
+     printf ("Enter a message to send:\n");
+     gets(encrypmessage);
+     l=strlen(encrypmessage);
+    
+     printf ("Enter a key:\n");
+     scanf ("%d", &shift);
     
     
-    
-    
-    return 0;
+for(i=0; i<l; i++)
+    {
+          if(encrypmessage[i]>='A' && encrypmessage[i]<='Z' )
+          {
+              encrypmessage[i]=(encrypmessage[i]-'A'+shift)%26 + 'A';
+              
+          } 
+          else if(encrypmessage[i]>='a' && encrypmessage[i]<= 'z'){
+           encrypmessage[i]= (encrypmessage[i]-'a'+ shift )%26+'a';
+     }
+ }
+
+     printf("%s\n",encrypmessage);
+}
+
+ return 0;
+ 
 }
