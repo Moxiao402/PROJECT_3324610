b2109ce9fffa2b05568d6564af8ea5d09874e4c8
Mingyi Shao <c3324610@uon.edu.au>
2019-04-11 12:08:44 +0000

    uploaded files from pc

--- blank.c
+++ blank.c
@@ -1,43 +1,16 @@
+#include "encrypter.h"
+
 #include <stdio.h>
-#include <string.h>
+#include <malloc.h>
+#include <memory.h>
 
-    char encrypmessage[100], decrypmessage[100];
-    int i,k,shift,l;
-    int choice1,choice2;
-     
-int main(){
+int main() {
+    char *source = "Letter frequencies, like word frequencies, tend to vary, both by writer and by subject. One cannot write an essay about x-rays without using frequent Xs, and the essay will have an idiosyncratic letter frequency if the essay is about the use of x-rays to treat zebras in Qatar. Different authors have habits which can be reflected in their use of letters. Hemingway's writing style, for example, is visibly different from Faulkner's. Letter, bigram, trigram, word frequencies, word length, and sentence length can be calculated for specific authors, and used to prove or disprove authorship of texts, even for authors whose styles are not so divergent.";
+    char *dest = malloc(sizeof(char) * strlen(source));
+    char *dest2 = malloc(sizeof(char) * strlen(source));
+    __encrypt_by_rotation_cipher(source, dest, 10, strlen(source));
+    printf("%s\n", dest);
 
-
-     printf("Please choose which from which task you would like me to conduct, and input its number:\n");
-     printf("1. Encryption\n");
-     printf("2. Decryption\n");
-    
-     scanf("%d", &choice1);
-     
-     if(choice1=1){
-     printf ("Enter a message to send:\n");
-     gets(encrypmessage);
-     l=strlen(encrypmessage);
-    
-     printf ("Enter a key:\n");
-     scanf ("%d", &shift);
-    
-    
-for(i=0; i<l; i++)
-    {
-          if(encrypmessage[i]>='A' && encrypmessage[i]<='Z' )
-          {
-              encrypmessage[i]=(encrypmessage[i]-'A'+shift)%26 + 'A';
-              
-          } 
-          else if(encrypmessage[i]>='a' && encrypmessage[i]<= 'z'){
-           encrypmessage[i]= (encrypmessage[i]-'a'+ shift )%26+'a';
-     }
- }
-
-     printf("%s\n",encrypmessage);
-}
-
- return 0;
- 
+    __decrypt_by_rotation_cipher_without_key(dest, dest2, strlen(source));
+    printf("%s\n", dest2);
 }
