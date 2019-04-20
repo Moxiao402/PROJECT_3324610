8bff311dfa9d956f1d216dddfd69ba2b8727e391
Moxiao402 <48337119+Moxiao402@users.noreply.github.com>
2019-04-20 12:22:07 +1000

    Add files via upload

--- encrypter.h
+++ encrypter.h
@@ -1,7 +1,7 @@
 #ifndef SIMPLEENCRYTER_ENCRYPTER_H
 #define SIMPLEENCRYTER_ENCRYPTER_H
 
-typedef short key_t;
+typedef short key_type;
 
 #define ROTATION_CIPHER_MOD 26
 #define ROTATION_CIPHER_TYPES 26
@@ -21,7 +21,7 @@ extern float letter_frequency_table[ALPHABET_LENGTH];
  * @param key the key of rotation cipher,a integer from 0 to 25(included)
  * @param len the length of message
  */
-extern void __encrypt_by_rotation_cipher(char *source, char *dest, key_t key, unsigned int len);
+extern void __encrypt_by_rotation_cipher(char *source, char *dest, key_type key, unsigned int len);
 
 /**
  * This function used to decrypt text by rotation cipher with the key.
@@ -30,7 +30,7 @@ extern void __encrypt_by_rotation_cipher(char *source, char *dest, key_t key, un
  * @param key the key of rotation cipher,a integer from 0 to 26(included)
  * @param len the length of cipher text
  */
-extern void __decrypt_by_rotation_cipher(char *source, char *dest, key_t key, unsigned int len);
+extern void __decrypt_by_rotation_cipher(char *source, char *dest, key_type key, unsigned int len);
 
 /**
  * The function used to calculate the key of specified cipher text encrypted by rotation cipher.
@@ -42,7 +42,7 @@ extern void __decrypt_by_rotation_cipher(char *source, char *dest, key_t key, un
  * @param len the length of cipher text
  * @return the key of this cipher text
  */
-extern key_t __calculate_rotation_cipher_key(char *source, unsigned int len);
+extern key_type __calculate_rotation_cipher_key(char *source, unsigned int len);
 
 /**
  * The function will be used to decrypt a cipher text encrypted by rotation cipher without key.
@@ -56,5 +56,41 @@ extern key_t __calculate_rotation_cipher_key(char *source, unsigned int len);
  * @param len the length of cipher text
  */
 extern void __decrypt_by_rotation_cipher_without_key(char *source, char *dest, unsigned int len);
+
+/**
+ * This function used to calculate the letter frequency of specified string
+ * @param source the pointer of specified string
+ * @param dest the pointer of an array of float which used to save the frequency
+ * @param len the length of specified string
+ */
+extern void __calculate_letter_frequency(char *source, float *dest, unsigned int len);
+
+/**
+ * This function used to encrypt a plain text by substitution cipher
+ * @param source the pointer of a plain text
+ * @param dest the pointer to save text after encrypt
+ * @param key the key of substitution, must be 26 letters
+ * @param len the length of the plain text
+ */
+extern void __encrypt_by_substitution_cipher(char *source, char *dest, char *key, unsigned int len);
+
+/**
+ * This function used to decrypt the cipher which decrypted by substitution cipher with the key
+ * @param source the pointer of cipher text
+ * @param dest the pointer to save text after decrypt
+ * @param key the key of substitution, must be 26 letters
+ * @param len the length of cipher text
+ */
+extern void __decrypt_by_substitution_cipher(char *source, char *dest, char *key, unsigned int len);
+
+/**
+ * This function used to decrypt the cipher which decrypted by substitution cipher with the key.
+ * In this function,I just calculate the frequency of every letter,and compare it with the frequency table of english words.
+ * @param source the pointer of a plain text
+ * @param dest the pointer to save text after encrypt
+ * @param len the length of the plain text
+ */
+extern void __decrypt_by_substitution_cipher_without_key(char *source, char *dest, unsigned int len);
+
 
 #endif //SIMPLEENCRYTER_ENCRYPTER_H
